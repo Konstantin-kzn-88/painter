d1cd7358f5e6cae4b4aee4b7c208b219c042a943
konstantin <kuznetsovkm@yandex.ru>
2022-07-03 10:36:02 +0300

    Painter. Рефактор geom через zip.

--- server/geom.py
+++ server/geom.py
@@ -1,25 +1,16 @@
 from shapely.geometry import LineString, Polygon
 
+"""
+Модуль geom предназначен для:
+ - вычисления расстояния между точками
+ - площади произвольного выпуклого многоугольника
+"""
+
 def lenght_for_line(data:list)->float:
-    i = 0
-    get_tuple = []
-    while i < len(data):
-        tuple_b = (float(data[i]), float(data[i + 1]))
-        get_tuple.append(tuple_b)
-        i += 2
-        if i == len(data):
-            break
-    length = LineString(get_tuple).length  # shapely
+    length = LineString(list(zip(*[iter(data)] * 2))).length  # shapely
     return length
 
 def area_for_poligon(data:list)->float:
-    i = 0
-    get_tuple = []
-    while i < len(data):
-        tuple_b = (float(data[i]), float(data[i + 1]))
-        get_tuple.append(tuple_b)
-        i += 2
-        if i == len(data):
-            break
-    area = Polygon(get_tuple).area  # shapely
+    area = Polygon(list(zip(*[iter(data)] * 2))).area  # shapely
     return area
+
